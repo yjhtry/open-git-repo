bd019940abf5aa11226b394ddde5db455e0132ac
yjh <860622588@qq.com>
2024-02-13 22:12:52 +0800

    chore: format

--- types/vscode.git.ts
+++ types/vscode.git.ts
@@ -148,67 +148,61 @@ export interface Repository {
   readonly state: RepositoryState
   readonly ui: RepositoryUIState
 
-  getConfigs(): Promise<{ key: string, value: string }[]>
-  getConfig(key: string): Promise<string>
-  setConfig(key: string, value: string): Promise<string>
-  getGlobalConfig(key: string): Promise<string>
+  getConfigs: () => Promise<{ key: string, value: string }[]>
+  getConfig: (key: string) => Promise<string>
+  setConfig: (key: string, value: string) => Promise<string>
+  getGlobalConfig: (key: string) => Promise<string>
 
-  getObjectDetails(treeish: string, path: string): Promise<{ mode: string, object: string, size: number }>
-  detectObjectType(object: string): Promise<{ mimetype: string, encoding?: string }>
-  buffer(ref: string, path: string): Promise<Buffer>
-  show(ref: string, path: string): Promise<string>
-  getCommit(ref: string): Promise<Commit>
+  getObjectDetails: (treeish: string, path: string) => Promise<{ mode: string, object: string, size: number }>
+  detectObjectType: (object: string) => Promise<{ mimetype: string, encoding?: string }>
+  buffer: (ref: string, path: string) => Promise<Buffer>
+  show: (ref: string, path: string) => Promise<string>
+  getCommit: (ref: string) => Promise<Commit>
 
-  add(paths: string[]): Promise<void>
-  revert(paths: string[]): Promise<void>
-  clean(paths: string[]): Promise<void>
+  add: (paths: string[]) => Promise<void>
+  revert: (paths: string[]) => Promise<void>
+  clean: (paths: string[]) => Promise<void>
 
-  apply(patch: string, reverse?: boolean): Promise<void>
-  diff(cached?: boolean): Promise<string>
-  diffWithHEAD(): Promise<Change[]>
-  diffWithHEAD(path: string): Promise<string>
-  diffWith(ref: string): Promise<Change[]>
-  diffWith(ref: string, path: string): Promise<string>
-  diffIndexWithHEAD(): Promise<Change[]>
-  diffIndexWithHEAD(path: string): Promise<string>
-  diffIndexWith(ref: string): Promise<Change[]>
-  diffIndexWith(ref: string, path: string): Promise<string>
-  diffBlobs(object1: string, object2: string): Promise<string>
-  diffBetween(ref1: string, ref2: string): Promise<Change[]>
-  diffBetween(ref1: string, ref2: string, path: string): Promise<string>
+  apply: (patch: string, reverse?: boolean) => Promise<void>
+  diff: (cached?: boolean) => Promise<string>
+  diffWithHEAD: (() => Promise<Change[]>) & ((path: string) => Promise<string>)
+  diffWith: ((ref: string) => Promise<Change[]>) & ((ref: string, path: string) => Promise<string>)
+  diffIndexWithHEAD: (() => Promise<Change[]>) & ((path: string) => Promise<string>)
+  diffIndexWith: ((ref: string) => Promise<Change[]>) & ((ref: string, path: string) => Promise<string>)
+  diffBlobs: (object1: string, object2: string) => Promise<string>
+  diffBetween: ((ref1: string, ref2: string) => Promise<Change[]>) & ((ref1: string, ref2: string, path: string) => Promise<string>)
 
-  hashObject(data: string): Promise<string>
+  hashObject: (data: string) => Promise<string>
 
-  createBranch(name: string, checkout: boolean, ref?: string): Promise<void>
-  deleteBranch(name: string, force?: boolean): Promise<void>
-  getBranch(name: string): Promise<Branch>
-  getBranches(query: BranchQuery, cancellationToken?: CancellationToken): Promise<Ref[]>
-  getBranchBase(name: string): Promise<Branch | undefined>
-  setBranchUpstream(name: string, upstream: string): Promise<void>
+  createBranch: (name: string, checkout: boolean, ref?: string) => Promise<void>
+  deleteBranch: (name: string, force?: boolean) => Promise<void>
+  getBranch: (name: string) => Promise<Branch>
+  getBranches: (query: BranchQuery, cancellationToken?: CancellationToken) => Promise<Ref[]>
+  getBranchBase: (name: string) => Promise<Branch | undefined>
+  setBranchUpstream: (name: string, upstream: string) => Promise<void>
 
-  getRefs(query: RefQuery, cancellationToken?: CancellationToken): Promise<Ref[]>
+  getRefs: (query: RefQuery, cancellationToken?: CancellationToken) => Promise<Ref[]>
 
-  getMergeBase(ref1: string, ref2: string): Promise<string>
+  getMergeBase: (ref1: string, ref2: string) => Promise<string>
 
-  tag(name: string, upstream: string): Promise<void>
-  deleteTag(name: string): Promise<void>
+  tag: (name: string, upstream: string) => Promise<void>
+  deleteTag: (name: string) => Promise<void>
 
-  status(): Promise<void>
-  checkout(treeish: string): Promise<void>
+  status: () => Promise<void>
+  checkout: (treeish: string) => Promise<void>
 
-  addRemote(name: string, url: string): Promise<void>
-  removeRemote(name: string): Promise<void>
-  renameRemote(name: string, newName: string): Promise<void>
+  addRemote: (name: string, url: string) => Promise<void>
+  removeRemote: (name: string) => Promise<void>
+  renameRemote: (name: string, newName: string) => Promise<void>
 
-  fetch(options?: FetchOptions): Promise<void>
-  fetch(remote?: string, ref?: string, depth?: number): Promise<void>
-  pull(unshallow?: boolean): Promise<void>
-  push(remoteName?: string, branchName?: string, setUpstream?: boolean, force?: ForcePushMode): Promise<void>
+  fetch: ((options?: FetchOptions) => Promise<void>) & ((remote?: string, ref?: string, depth?: number) => Promise<void>)
+  pull: (unshallow?: boolean) => Promise<void>
+  push: (remoteName?: string, branchName?: string, setUpstream?: boolean, force?: ForcePushMode) => Promise<void>
 
-  blame(path: string): Promise<string>
-  log(options?: LogOptions): Promise<Commit[]>
+  blame: (path: string) => Promise<string>
+  log: (options?: LogOptions) => Promise<Commit[]>
 
-  commit(message: string, opts?: CommitOptions): Promise<void>
+  commit: (message: string, opts?: CommitOptions) => Promise<void>
 }
 
 export interface RemoteSource {
@@ -221,15 +215,15 @@ export interface RemoteSourceProvider {
   readonly name: string
   readonly icon?: string // codicon name
   readonly supportsQuery?: boolean
-  getRemoteSources(query?: string): ProviderResult<RemoteSource[]>
-  getBranches?(url: string): ProviderResult<string[]>
-  publishRepository?(repository: Repository): Promise<void>
+  getRemoteSources: (query?: string) => ProviderResult<RemoteSource[]>
+  getBranches?: (url: string) => ProviderResult<string[]>
+  publishRepository?: (repository: Repository) => Promise<void>
 }
 
 export interface RemoteSourcePublisher {
   readonly name: string
   readonly icon?: string // codicon name
-  publishRepository(repository: Repository): Promise<void>
+  publishRepository: (repository: Repository) => Promise<void>
 }
 
 export interface Credentials {
@@ -238,20 +232,20 @@ export interface Credentials {
 }
 
 export interface CredentialsProvider {
-  getCredentials(host: Uri): ProviderResult<Credentials>
+  getCredentials: (host: Uri) => ProviderResult<Credentials>
 }
 
 export interface PostCommitCommandsProvider {
-  getCommands(repository: Repository): Command[]
+  getCommands: (repository: Repository) => Command[]
 }
 
 export interface PushErrorHandler {
-  handlePushError(
+  handlePushError: (
     repository: Repository,
     remote: Remote,
     refspec: string,
     error: Error & { gitErrorCode: GitErrorCodes },
-  ): Promise<boolean>
+  ) => Promise<boolean>
 }
 
 export interface BranchProtection {
@@ -266,17 +260,17 @@ export interface BranchProtectionRule {
 
 export interface BranchProtectionProvider {
   onDidChangeBranchProtection: Event<Uri>
-  provideBranchProtection(): BranchProtection[]
+  provideBranchProtection: () => BranchProtection[]
 }
 
 export interface CommitMessageProvider {
   readonly title: string
   readonly icon?: Uri | { light: Uri, dark: Uri } | ThemeIcon
-  provideCommitMessage(
+  provideCommitMessage: (
     repository: Repository,
     changes: string[],
     cancellationToken?: CancellationToken,
-  ): Promise<string | undefined>
+  ) => Promise<string | undefined>
 }
 
 export type APIState = 'uninitialized' | 'initialized'
@@ -295,18 +289,18 @@ export interface API {
   readonly onDidOpenRepository: Event<Repository>
   readonly onDidCloseRepository: Event<Repository>
 
-  toGitUri(uri: Uri, ref: string): Uri
-  getRepository(uri: Uri): Repository | null
-  init(root: Uri, options?: InitOptions): Promise<Repository | null>
-  openRepository(root: Uri): Promise<Repository | null>
+  toGitUri: (uri: Uri, ref: string) => Uri
+  getRepository: (uri: Uri) => Repository | null
+  init: (root: Uri, options?: InitOptions) => Promise<Repository | null>
+  openRepository: (root: Uri) => Promise<Repository | null>
 
-  registerRemoteSourcePublisher(publisher: RemoteSourcePublisher): Disposable
-  registerRemoteSourceProvider(provider: RemoteSourceProvider): Disposable
-  registerCredentialsProvider(provider: CredentialsProvider): Disposable
-  registerPostCommitCommandsProvider(provider: PostCommitCommandsProvider): Disposable
-  registerPushErrorHandler(handler: PushErrorHandler): Disposable
-  registerBranchProtectionProvider(root: Uri, provider: BranchProtectionProvider): Disposable
-  registerCommitMessageProvider(provider: CommitMessageProvider): Disposable
+  registerRemoteSourcePublisher: (publisher: RemoteSourcePublisher) => Disposable
+  registerRemoteSourceProvider: (provider: RemoteSourceProvider) => Disposable
+  registerCredentialsProvider: (provider: CredentialsProvider) => Disposable
+  registerPostCommitCommandsProvider: (provider: PostCommitCommandsProvider) => Disposable
+  registerPushErrorHandler: (handler: PushErrorHandler) => Disposable
+  registerBranchProtectionProvider: (root: Uri, provider: BranchProtectionProvider) => Disposable
+  registerCommitMessageProvider: (provider: CommitMessageProvider) => Disposable
 }
 
 export interface GitExtension {
@@ -323,5 +317,5 @@ export interface GitExtension {
    * @param version Version number.
    * @returns API instance
    */
-  getAPI(version: 1): API
+  getAPI: (version: 1) => API
 }
